bd5432c55f0511035e6aa96b7ead343f4b10b68e
Claire-Elise Hochet <cehochet@emi.u-bordeaux1.fr>
2019-05-09 18:07:24 +0200

    fix typo

--- src/main/java/IndexController.java
+++ src/main/java/IndexController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class IndexController {
 
     @GetMapping("/")
-    public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+    public String index(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
         model.addAttribute("name", name);
         return "index";
     }
