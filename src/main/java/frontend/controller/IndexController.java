c690b14691856e3a8a26a460eddd1f297df63db2
Claire-Elise <claire-elise.hochet@etu.u-bordeaux.fr>
2019-05-14 18:37:06 +0200

    Ajout CSS Ajout Fragments Ajout html

--- src/main/java/frontend/controller/IndexController.java
+++ src/main/java/frontend/controller/IndexController.java
@@ -11,6 +11,6 @@ public class IndexController {
     @GetMapping("/")
     public String index(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
         model.addAttribute("name", name);
-        return "index";
+        return "home";
     }
 }
